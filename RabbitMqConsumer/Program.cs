d39e58e96d740c3215544156091944dc126be470
Udson Souza da Cruz <hudysouza@gmail.com>
2019-10-22 14:05:47 -0300

    first commit

--- RabbitMqConsumer/Program.cs
+++ RabbitMqConsumer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace RabbitMqConsumer
 {
@@ -6,7 +7,31 @@ namespace RabbitMqConsumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var rabbit = Serget.Tools.Queueing.QueueBusFactory.Create("200.196.226.148", null, null))
+            {
+                const string QueueName = "Serget.Indicacao.Exception";
+                rabbit.QueueName = QueueName;
+                var obj = rabbit.ReceiveMessage<object>();
+                int count = 0;
+
+                Console.WriteLine(string.Format("Lendo mensagem das fila {0} ....", QueueName));
+                while (obj != null)
+                {
+                    
+                    Console.WriteLine("Carregando mensagem para arquivo de texto");
+                    string message = obj.ToString();
+                    Directory.CreateDirectory(string.Format(@"c:/jsonfiles/rabbitmq/{0}", QueueName));
+                    var filename = string.Format(@"c:/jsonfiles/rabbitmq/{0}/{1}.txt", QueueName, Guid.NewGuid());
+                    File.WriteAllText(filename, message);
+                    Console.WriteLine(string.Format("Gerado arquivo: {0}", filename));
+                    obj = rabbit.ReceiveMessage<object>();
+                    count++;
+                }
+
+                Console.WriteLine(string.Format("Foram gerados {0} arquivos referente a fila {1}", count, QueueName));
+            }
+            
+            Console.Read();
         }
     }
 }
